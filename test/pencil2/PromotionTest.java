12bcc7d5afb53f78520034a7d8c93d115abad0fd
TonyS <anthony.sterrett@gmail.com>
2016-11-14 14:05:12 -0500

    Added testPriceRedcition.  Move Promotion create in to setup().

--- test/pencil2/PromotionTest.java
+++ test/pencil2/PromotionTest.java
@@ -17,38 +17,60 @@ import static org.junit.Assert.*;
  * @author tonys
  */
 public class PromotionTest {
-    
+
+    Promotion instance;
     public PromotionTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        instance = new Promotion();        
     }
-    
+
     @After
     public void tearDown() {
     }
 
     @Test
     public void testInitialStableDays() {
-        System.out.println ("testInitialStableDays");
+        System.out.println("testInitialStableDays");
         int days = 0;
-        Promotion instance  = new Promotion();
         int expectedResult = 29;
         instance.InitialStableDays(29);
         int result = instance.stableDays;
-        
-        assertEquals (expectedResult, result);
 
-       
+        assertEquals(expectedResult, result);
     }
-    
+
+    @Test
+    public void testInitialPrice() {
+        System.out.println("testPrice");
+        Float price = 0.0F;
+        Float expectedResult = 10.0F;
+        instance.InitialPrice(10.0F);
+        Float result = instance.price;
+
+        assertEquals(expectedResult, result, 0.01);
+    }
+
+    @Test
+    public void testPriceReduction() {
+        System.out.println("priceReduction");
+        Float initPrice = 10.0F;
+        instance.InitialPrice(initPrice);
+        int days = 31;
+        instance.InitialStableDays(days);
+        Boolean result = instance.priceReduction(9.00F);
+        
+        assertTrue("Promotion", result);
+    }
+
 }
