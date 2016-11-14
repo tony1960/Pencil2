a3987d2794c0623462f1127dc19fdb649e0129cf
TonyS <anthony.sterrett@gmail.com>
2016-11-14 14:05:51 -0500

    Added priceRedcition.  Move Promotion create in to setup().

--- src/pencil2/Promotion.java
+++ src/pencil2/Promotion.java
@@ -10,23 +10,52 @@ package pencil2;
  * @author tonys
  */
 public class Promotion {
-    
-    Float   price        =   0.0F;
-    Float   reducedPrice =   0.0F;
-    int     stableDays   =   0;
-    
 
-/**
- * InitialStableDays. Sets the number of days at a Stable price.
- * 
- * @param   days    Stable days.
- */    
-   public void  InitialStableDays(int stableDays){
-       this.stableDays = stableDays;
-   }
-    
-    
+    Float price = 0.0F;
+    Float reducedPrice = 0.0F;
+    int stableDays = 0;
 
-    
-    
+    /**
+     * InitialStableDays. Sets the number of days at a Stable price.
+     *
+     * @param days Stable days.
+     */
+    public void InitialStableDays(int stableDays) {
+        this.stableDays = stableDays;
+    }
+
+    /**
+     * InitialPrice. Sets the Price for start of test.
+     *
+     * @param   price   Price value.
+     * @return  true    - if reducted price is with range 
+     * @return false    - if reducted price is out of range.
+     */
+    public void InitialPrice(Float price) {
+        this.price = price;
+    }
+
+    public Boolean priceReduction(Float reductedPrice) {
+
+        if (stableDays < 30) {
+            return false;
+        }
+
+        Float low;
+        Float high;
+
+        low = price * .70F;
+        high = price * .95F;
+
+        if (reductedPrice.compareTo(low) < 0) {
+            return false;
+        }
+        if (reductedPrice.compareTo(high) > 0) {
+            return false;
+        }
+        this.reducedPrice = reductedPrice;
+
+        return true;
+    }
+
 }
