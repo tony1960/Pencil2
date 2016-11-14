e891c3bd86653f986e79ab6aa13282793f583e29
TonyS <anthony.sterrett@gmail.com>
2016-11-14 12:54:39 -0500

    Initial Build in promotion. First successful test

---
+++ src/pencil2/Pencil2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pencil2;
+
+/**
+ *
+ * @author tonys
+ */
+public class Pencil2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // stub
+    }
+    
+}
